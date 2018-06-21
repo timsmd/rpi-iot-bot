9e5f149e1285a54f6d9a9932c9d333c1d6d8143a
Timofey Nazarov <timothy96@rambler.ru>
2018-06-21 13:56:43 -0400

    flask powered blink implemented

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from RPi import GPIO as gpio
+
+gpio.setmode(gpio.BCM)
+gpio.setup(2, gpio.OUT)
+gpio.setup(5, gpio.OUT)
+gpio.output(5, gpio.HIGH)
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
+def hello():
+    return render_template("index.html")
+
+@app.route('/ledon')
+def ledon():
+    gpio.output(2, gpio.HIGH)
+    return(500)
+
+@app.route('/ledoff')
+def ledoff():
+    gpio.output(2, gpio.LOW)
+    return(500)
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.60')
